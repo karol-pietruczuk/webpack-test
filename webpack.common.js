f6ddaf6d5595fdfe3adfc5229012886b1db92ca4
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2020-09-11 12:59:36 +0200

    new file: karol.html

--- webpack.common.js
+++ webpack.common.js
@@ -88,5 +88,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/karol.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "karol.html",
+    }),
   ],
 };
